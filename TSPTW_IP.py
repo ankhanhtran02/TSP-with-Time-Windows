6c8376f1e8a7ee212d84afca816c2c692cbc67ef
Decent_B <78393122+Decent-Cypher@users.noreply.github.com>
2023-12-27 19:54:29 +0700

    Update TSPTW_IP.py

--- TSPTW_IP.py
+++ TSPTW_IP.py
@@ -1,79 +1,98 @@
 from ortools.linear_solver import pywraplp
+from test_generator import *
+from urllib.request import urlopen
+from timeit import default_timer
+
 BIG_M = 9999999
 
-def solve():
+
+def solve(problem_inputs):
     # Create the linear solver with the GLOP backend
-    solver = pywraplp.Solver.CreateSolver("SCIP")
+    n, e, l, d, t = problem_inputs
+    e = [None] + e
+    l = [None] + l
+    d = [0] + d
+    solver = pywraplp.Solver.CreateSolver("SAT")
+    solver.SetTimeLimit(180*1000)
     if not solver:
         return
-    # Input for the program
-    n = int(input())
-    e, l, d = [None], [None], [0]
-    for _ in range(1, n+1):
-        e_i, l_i, d_i = map(int, input().split())
-        e.append(e_i)
-        l.append(l_i)
-        d.append(d_i)
-    t = []
-    for _ in range(n+1):
-        t.append(list(map(int, input().split())))
-    
+
+    start_solving = default_timer()
+    TSP_path = []
+
     # Create variables
-    x = [[solver.IntVar(0, 1 - int(i==j), f'x_{i}_{j}') for j in range (n+1)] for i in range(n+1)] # x_i_j = 1 if we go from point i to point j, else it is 0
-    times_passed = [solver.IntVar(0, 0, f'times_passed_0')]
-    for i in range(1, n+1):
-        times_passed.append(solver.IntVar(e[i], l[i], f'times_passed_{i}'))
+    # x = [
+    #     [solver.IntVar(0, 1 - int(i == j), f"x_{i}_{j}") for j in range(n + 1)]
+    #     for i in range(n + 1)
+    # ]  
+    # x_i_j = 1 if we go from point i to point j, else it is 0
+    x = []
+    for i in range(n+1):
+        temp = []
+        for j in range(n+1):
+            if j==i or (i >= 1 and j >= 1 and e[i] > l[j]):
+                temp.append(solver.IntVar(0, 0, f"x_{i}_{j}"))
+            else:
+                temp.append(solver.IntVar(0, 1, f"x_{i}_{j}"))
+        x.append(temp)
+    times_passed = [solver.IntVar(0, 0, f"times_passed_0")]
+    for i in range(1, n + 1):
+        times_passed.append(solver.IntVar(e[i], l[i], f"times_passed_{i}"))
     # times_passed.append(solver.IntVar(0, solver.infinity(), f'times_passed{n+1}'))
-    y = [None] + [solver.IntVar(0, 1, f'y_{i}') for i in range(1, n+1)]
+    y = [None] + [solver.IntVar(0, 1, f"y_{i}") for i in range(1, n + 1)]
 
     # Adding constraints
-    for i in range(n+1):
-        ct = solver.Constraint(1, 1, f'ct_row_{i}')
-        for j in range(n+1):
+    for i in range(n + 1):
+        ct = solver.Constraint(1, 1, f"ct_row_{i}")
+        for j in range(n + 1):
             ct.SetCoefficient(x[i][j], 1)
-    for j in range(n+1):
-        ct = solver.Constraint(1, 1, f'ct_col_{j}')
-        for i in range(n+1):
+    for j in range(n + 1):
+        ct = solver.Constraint(1, 1, f"ct_col_{j}")
+        for i in range(n + 1):
             ct.SetCoefficient(x[i][j], 1)
 
-    for i in range(n+1):
-        for j in range(1, n+1):
-            if j != i:
-                ct1 = solver.Constraint(-solver.infinity(), BIG_M - e[j], f'ct_{i}_{j}_1')
-                ct1.SetCoefficient(x[i][j], BIG_M)
-                ct1.SetCoefficient(times_passed[j], -1)
+    for i in range(n + 1):
+        for j in range(1, n + 1):
+            if j != i and not (i >= 1 and e[i] > l[j]):
+                # ct1 = solver.Constraint(
+                #     -solver.infinity(), BIG_M - e[j], f"ct_{i}_{j}_1"
+                # )
+                # ct1.SetCoefficient(x[i][j], BIG_M)
+                # ct1.SetCoefficient(times_passed[j], -1)
 
-
-                ct2 = solver.Constraint(-solver.infinity(), BIG_M - d[i] - t[i][j], f'ct_{i}_{j}_2')
+                ct2 = solver.Constraint(
+                    -solver.infinity(), BIG_M - d[i] - t[i][j], f"ct_{i}_{j}_2"
+                )
                 ct2.SetCoefficient(x[i][j], BIG_M)
                 ct2.SetCoefficient(times_passed[j], -1)
                 ct2.SetCoefficient(times_passed[i], 1)
 
+                # ct3 = solver.Constraint(
+                #     -solver.infinity(), 2 * BIG_M + e[j], f"ct_{i}_{j}_3"
+                # )
+                # ct3.SetCoefficient(x[i][j], BIG_M)
+                # ct3.SetCoefficient(times_passed[j], 1)
+                # ct3.SetCoefficient(y[j], BIG_M)
 
-                ct3 = solver.Constraint(-solver.infinity(), 2 * BIG_M + e[j], f'ct_{i}_{j}_3')
-                ct3.SetCoefficient(x[i][j], BIG_M)
-                ct3.SetCoefficient(times_passed[j], 1)
-                ct3.SetCoefficient(y[j], BIG_M)
-
-
-                ct4 = solver.Constraint(-solver.infinity(), BIG_M + d[i] + t[i][j], f'ct_{i}_{j}_4')
-                ct4.SetCoefficient(x[i][j], BIG_M)
-                ct4.SetCoefficient(times_passed[j], 1)
-                ct4.SetCoefficient(times_passed[i], -1)
-                ct4.SetCoefficient(y[j], -BIG_M)
-            
-
+                # ct4 = solver.Constraint(
+                #     -solver.infinity(), BIG_M + d[i] + t[i][j], f"ct_{i}_{j}_4"
+                # )
+                # ct4.SetCoefficient(x[i][j], BIG_M)
+                # ct4.SetCoefficient(times_passed[j], 1)
+                # ct4.SetCoefficient(times_passed[i], -1)
+                # ct4.SetCoefficient(y[j], -BIG_M)
 
     # Set solver objective
     objective = solver.Objective()
-    for i in range(n+1):
-        for j in range(n+1):
-            if j != 0:
+    for i in range(n + 1):
+        for j in range(n + 1):
+            if j!=0:
                 objective.SetCoefficient(x[i][j], t[i][j])
     objective.SetMinimization()
     # Solving
     status = solver.Solve()
     if status == pywraplp.Solver.OPTIMAL or status == pywraplp.Solver.FEASIBLE:
+        print(f'ORTOOLS status is {status}')
         # print(objective.Value())
         # print(solver.wall_time())
         # for i in range(n+1):
@@ -83,19 +102,64 @@ def solve():
         i = 0
         print(n)
         while True:
-            for j in range(n+1):
+            for j in range(n + 1):
                 if x[i][j].solution_value() == 1:
                     if j != 0:
-                        print(j, end=' ')
+                        print(j, end=" ")
+                        TSP_path.append(j)
                     i = j
                     break
             if i == 0:
                 print()
                 break
-        
+    elif status == pywraplp.Solver.NOT_SOLVED:
+        print('BUG VL')
+        i = 0
+        print(n)
+        while True:
+            for j in range(n + 1):
+                if x[i][j].solution_value() == 1:
+                    if j != 0:
+                        print(j, end=" ")
+                        TSP_path.append(j)
+                    i = j
+                    break
+            if i == 0:
+                print()
+                break
     else:
         print(status)
+    end_solving = default_timer()
+    print(f"Execution time is: {end_solving - start_solving}")
+    return TSP_path
+
 
 if __name__ == "__main__":
-    solve()
+    # Input for the program
+    # n = int(input())
+    # e, l, d = [None], [None], [0]
+    # for _ in range(1, n+1):
+    #     e_i, l_i, d_i = map(int, input().split())
+    #     e.append(e_i)
+    #     l.append(l_i)
+    #     d.append(d_i)
+    # t = []
+    # for _ in range(n+1):
+    #     t.append(list(map(int, input().split())))
+
+    # base_URL = "https://raw.githubusercontent.com/ankhanhtran02/TSP-with-Time-Windows/main/test_cases/"
+    # test_file_name = "rbg010a.txt"
+    # txt = urlopen(base_URL + test_file_name).read().decode("utf-8")
+    # with open("TSPTW_test_1.txt", "w") as file:
+    #     file.write(txt)
+
+    print("Start solving...")
+    filename = 'new_test_cases/B30_2.txt'
+    TSP_path = solve(read_input_file(filename))
+    if len(TSP_path) > 0:
+        print(TSP_path)
+        print(f'The optimal cost is: {TSPTW_cost(read_input_file(filename),TSP_path)}')
+    else:
+        print(f'Cannot find any feasible solution in time limit.')
+
 
